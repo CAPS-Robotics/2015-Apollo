15db9dff98c70a09b10eb29879842bb4546f5ac5
Tianyi Ma <tn.ma@outlook.com>
2015-01-08 14:30:10 -0600

    Formatting
    What happened to it…

--- src/Robot.cpp
+++ src/Robot.cpp
@@ -2,59 +2,52 @@
 #include "Robot.h"
 #include "config.h"
 
-Seabiscuit::~Seabiscuit()
-{
-    delete drive;
-    delete joystick;
-    delete gyro;
+Seabiscuit::~Seabiscuit() {
+	delete drive;
+	delete joystick;
+	delete gyro;
 }
 
-void Seabiscuit::RobotInit()
-{
-    drive = new RobotDrive(LEFT_MOTOR_PWM, RIGHT_MOTOR_PWM);
-    joystick = new Joystick(JOY_PORT_1);
-    gyro = new Gyro(GYRO_CHANNEL);
+void Seabiscuit::RobotInit() {
+	drive = new RobotDrive(LEFT_MOTOR_PWM, RIGHT_MOTOR_PWM);
+	joystick = new Joystick(JOY_PORT_1);
+	gyro = new Gyro(GYRO_CHANNEL);
 
-    drive->SetSafetyEnabled(false);
+	drive->SetSafetyEnabled(false);
 
-    pthread_create(&driveThread, NULL, driveFunc, NULL);
+	pthread_create(&driveThread, NULL, driveFunc, NULL);
 }
 
-void Seabiscuit::TeleopInit()
-{
-    driveRun = true;
+void Seabiscuit::TeleopInit() {
+	driveRun = true;
 }
 
-void Seabiscuit::DisabledInit()
-{
-    driveRun = false;
+void Seabiscuit::DisabledInit() {
+	driveRun = false;
 }
 
-void* driveFunc(void* arg)
-{
+void* driveFunc(void* arg) {
 	float stickDif;
 	float stickAvg;
 	float gyroAngle;
-    while(1 != 2)
-    {
-        //drive with tank drive for now
-        if(driveRun)
-        {
-        	stickDif = joystick->GetRawAxis(JOY_AXIS_LY) - joystick->GetRawAxis(JOY_AXIS_RY);
-        	gyro->Reset();
-    		//gyro based drive for straightness if sticks have difference of +- 3
-        	while (stickDif <= 3 && stickDif >= -3) {
-            	gyroAngle = gyro->GetAngle();
-            	stickAvg = (joystick->GetRawAxis(JOY_AXIS_LY) + joystick->GetRawAxis(JOY_AXIS_RY)) / 2.f;
-            	drive->TankDrive(((stickAvg / 128.f) * 100) - gyroAngle, ((stickAvg / 128.f) * 100) + gyroAngle);
-            	stickDif = joystick->GetRawAxis(JOY_AXIS_LY) - joystick->GetRawAxis(JOY_AXIS_RY);
-        	}
-        	drive->TankDrive((joystick->GetRawAxis(JOY_AXIS_LY) / 128.f) * 100, (joystick->GetRawAxis(JOY_AXIS_RY) / 128.f) * 100);
-        }
+	while (1 != 2) {
+		//drive with tank drive for now
+		if (driveRun) {
+			stickDif = joystick->GetRawAxis(JOY_AXIS_LY) - joystick->GetRawAxis(JOY_AXIS_RY);
+			gyro->Reset();
+			//gyro based drive for straightness if sticks have difference of +- 3
+			while (stickDif <= 3 && stickDif >= -3) {
+				gyroAngle = gyro->GetAngle();
+				stickAvg = (joystick->GetRawAxis(JOY_AXIS_LY) + joystick->GetRawAxis(JOY_AXIS_RY)) / 2.f;
+				drive->TankDrive(((stickAvg / 128.f) * 100) - gyroAngle, ((stickAvg / 128.f) * 100) + gyroAngle);
+				stickDif = joystick->GetRawAxis(JOY_AXIS_LY) - joystick->GetRawAxis(JOY_AXIS_RY);
+			}
+			drive->TankDrive((joystick->GetRawAxis(JOY_AXIS_LY) / 128.f) * 100, (joystick->GetRawAxis(JOY_AXIS_RY) / 128.f) * 100);
+		}
 
-        //syncing crap
-        Wait(0.01);
-    }
+		//syncing crap
+		Wait(0.01);
+	}
 }
 
 START_ROBOT_CLASS(Seabiscuit);
